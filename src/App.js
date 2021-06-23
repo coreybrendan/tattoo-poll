639a52310cd5a32dda62d89fa266c4626dc87a77
coreybrendan <coreybrendan@me.com>
2021-06-23 18:10:20 -0400

    styled and finished footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import rightSwallow from './assets/right-swallow.png';
 import lucky from './assets/lucky.png';
 import sailor from './assets/sailor.png';
 import panther from './assets/panther-head.png';
+import footerArt from './assets/footer-art.png'
 
 function App() {
   // data stored in state
@@ -60,42 +61,50 @@ function App() {
         <img className="left-swallow" src={leftSwallow} alt="a traditional swallow tattoo facing right" />
         <img className="right-swallow" src={rightSwallow} alt="a traditional swallow tattoo facing left" />
       </header>
-      <section className="form-container wrapper">
-        <p>Shops are opening in Toronto, the summer months are finally here and warmer weather means less clothing and more skin. Naturally, that signals tattoo season is upon us. Help me decide my next tattoo from the options below!</p>
-        <form onSubmit={(event) => {addEmUp(event, userChoice)}} action="submit" type="radio" name="tattoo-poll">
-          <input onChange={handleUserChoice} type="radio" id="tattoo1" name="tattoo-poll" value={userChoice} />
-          <label className="grid-lucky" htmlFor="tattoo1">
-            <img className="lucky-tatt" src={lucky} alt="a traditional tattoo with a lucky banner and four playing cards" />
-          </label>
+      <main>
+        <section className="form-container wrapper">
+          <p>Shops are opening in Toronto, the summer months are finally here, and warmer weather means less clothing and more skin. Naturally, that signals tattoo season is upon us. Help me decide my next tattoo from the options below!</p>
+          <form onSubmit={(event) => {addEmUp(event, userChoice)}} action="submit" type="radio" name="tattoo-poll">
+            <input onChange={handleUserChoice} type="radio" id="tattoo1" name="tattoo-poll" value={userChoice} />
+            <label className="grid-lucky" htmlFor="tattoo1">
+              <img className="lucky-tatt" src={lucky} alt="a traditional tattoo with a lucky banner and four playing cards" />
+            </label>
 
-          <input onChange={handleUserChoice} type="radio" id="tattoo2" name="tattoo-poll" value={userChoice} />
-          <label className="grid-sailor" htmlFor="tattoo2">
-            <img className="sailor-tatt" src={sailor} alt="a traditional tattoo of a sailor framed with rope and an anchor" />
-          </label>
+            <input onChange={handleUserChoice} type="radio" id="tattoo2" name="tattoo-poll" value={userChoice} />
+            <label className="grid-sailor" htmlFor="tattoo2">
+              <img className="sailor-tatt" src={sailor} alt="a traditional tattoo of a sailor framed with rope and an anchor" />
+            </label>
 
-          <input onChange={handleUserChoice} type="radio" id="tattoo3" name="tattoo-poll" value={userChoice} />
-          <label className="grid-panther" htmlFor="tattoo3">
-            <img className="panther-tatt" src={panther} alt="a traditional tattoo of a panther head growling" />
-          </label>
+            <input onChange={handleUserChoice} type="radio" id="tattoo3" name="tattoo-poll" value={userChoice} />
+            <label className="grid-panther" htmlFor="tattoo3">
+              <img className="panther-tatt" src={panther} alt="a traditional tattoo of a panther head growling" />
+            </label>
 
-          <button className="grid-button" type="submit">Lock It In?!</button>
-        </form>
-      </section>
-      <section className="results-container">
-        <ul>
-        <h2>Track The Live Results:</h2>
-          {
-            tattoos.map((tattoo) => {
-              // display tattoo name and count data
-              return (
-                <li key={tattoo.key}>
-                  {tattoo.name.name}: {tattoo.name.count}
-                </li>
-              )
-            })
-          }
-        </ul>
-      </section>
+            <button className="grid-button" type="submit">Lock It In?!</button>
+          </form>
+        </section>
+        <section className="results-container">
+          <ul>
+          <h2>Track The Results Live:</h2>
+            {
+              tattoos.map((tattoo) => {
+                // display tattoo name and count data
+                return (
+                  <li key={tattoo.key}>
+                    {tattoo.name.name}: {tattoo.name.count}
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <p>Crafted by Corey Sheldrick</p>
+        <p>at Juno College of Technology</p>
+        <p>in Toronto, Ontario.</p>
+        <img src={footerArt} alt="An island scene with palm trees, and mountains." />
+      </footer>
     </div>
   );
 }
